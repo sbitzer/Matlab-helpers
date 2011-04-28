d86d1f72f91bf8818632b0440eaa075e7d283174
Sebastian Bitzer <bitzer@cbs.mpg.de>
2011-04-28 19:41:44 +0200

    made plotMeanWithStd compatible with single trial data corrected error in description

--- figures/plotMeanWithStd.m
+++ figures/plotMeanWithStd.m
@@ -8,7 +8,7 @@
 %                   samples are in rows
 %                   [ndps,nt] = size
 %       xx      -   x-positions of data points
-%                   [ndps,1] = size, [default: (1:nt)']
+%                   [nt,1] = size, [default: (1:nt)']
 %       vis     -   visualisation structure containing handles to
 %                   graphics objects, only used if has field .fig which is
 %                   then used to plot into the given figure
@@ -22,34 +22,38 @@ function vis = plotMeanWithStd(data,xx,vis)
 
 [ndps,nt] = size(data);
 
-if nargin<2 || isempty(xx)
-    xx = (1:nt)';
-end
-
-mu = mean(data);
-stdev = std(data);
-
 if nargin==3
     vis = initvis(vis);
 else
     vis = initvis([]);
 end
+hold on
 if ~isfield(vis,'mean')
     vis.mean = [];
     vis.stdshade = [];
 end
 
-X = [xx;xx(end:-1:1)];
-Y = [mu+2*stdev, mu(end:-1:1)-2*stdev(end:-1:1)]';
-vis.mean(end+1) = plot(xx,mu,'k');
-hold on
-if ndps>1
-    vis.stdshade(end+1) = fill(X,Y,[.9 .9 .9],'LineStyle','none','EdgeColor',[.7 .7 .7]);
-else
-    warning('Only plotting one trial.')
-    vis.stdshade(end+1) = [];
+if nargin<2 || isempty(xx)
+    xx = (1:nt)';
 end
 
+if ndps==1
+    warning('matlabHelpers:singleTrial','Only plotting one trial.')
+    mu = data;
+    shaded = false;
+else
+    mu = mean(data);
+    stdev = std(data);
+    
+    X = [xx;xx(end:-1:1)];
+    Y = [mu+2*stdev, mu(end:-1:1)-2*stdev(end:-1:1)]';
+    vis.stdshade(end+1) = fill(X,Y,[.9 .9 .9],'LineStyle','none','EdgeColor',[.7 .7 .7]);
+    shaded = true;
+end
+vis.mean(end+1) = plot(xx,mu,'k');
+
 % make sure the shading is in background
-handles = get(gca,'Children');
-set(gca,'Children',[handles(2:end);handles(1)]);
+if shaded
+    handles = get(gca,'Children');
+    set(gca,'Children',handles([1,3:end,2]));
+end
